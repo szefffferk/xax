edfd01600c59b7a15b627e79fa0dd3c43971e33f
szefffferk <kamilbarkowski2@gmail.com>
2025-11-15 22:46:58 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,3 @@
-<?php
-session_start();
-$_SESSION['loggedin'] = true;
-$_SESSION['username'] = "auto";
-header("Location: dashboard.php");
-exit;
-?>
+$users = array(
+    "twojlogin" => "twojehaslo",
+);
