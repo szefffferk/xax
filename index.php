28cdfc3451bd0ee1aa5b359504c8493c83d9a989
szefffferk <kamilbarkowski2@gmail.com>
2025-11-15 22:21:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-// Automatyczne przekierowanie do dashboard bez logowania
+// Wyczyść całkowicie sesję
+session_start();
+session_unset();
+session_destroy();
+
+// Usuń cookie sesji
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time()-3600, '/');
+}
+
+// Przekieruj do dashboard bez logowania
 header("Location: dashboard.php");
 exit;
 ?>
