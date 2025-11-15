21cc0efc52bda4fae683b5ac7f9ec868dcede995
szefffferk <kamilbarkowski2@gmail.com>
2025-11-15 22:24:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,76 @@
-<?php
-// Wyczyść całkowicie sesję
-session_start();
-session_unset();
-session_destroy();
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>demObywatel | Dashboard </title>
+    <link rel="stylesheet" href="dashboard.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <style>
+        .loader {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: #121212;
+            transition: opacity 0.75s, visibility 0.75s;
+        }
 
-// Usuń cookie sesji
-if (isset($_COOKIE[session_name()])) {
-    setcookie(session_name(), '', time()-3600, '/');
-}
+        .loader--hidden {
+            opacity: 0;
+            visibility: hidden;
+        }
 
-// Przekieruj do dashboard bez logowania
-header("Location: dashboard.php");
-exit;
-?>
+        .loader::after {
+            content: "";
+            width: 75px;
+            height: 75px;
+            border: 15px solid #dddddd;
+            border-top-color: #1e1e1e;
+            border-radius: 50%;
+            animation: loading 0.75s ease infinite;
+        }
+
+        @keyframes loading {
+            from {
+                transform: rotate(0turn);
+            }
+            to {
+                transform: rotate(1turn);
+            }
+        }
+    </style>
+    <script>
+        window.addEventListener("load", () => {
+            const loader = document.querySelector(".loader");
+
+            loader.classList.add("loader--hidden");
+
+            loader.addEventListener("transitionend", () => {
+                document.body.removeChild(loader);
+            });
+        });
+    </script>
+</head>
+<body>
+<div class="loader"></div>
+<header>
+    <div class="header-left">
+        <h1>demObywatel | Dashboard</h1>
+        <p>Dostęp bez logowania</p>
+    </div>
+    <div class="header-right">
+    </div>
+</header>
+<div class="content">
+    <div class="generator-container">
+        <h3>mObywatel 2.0</h3>
+        <a href="generator.php" id="generator-btn">Generator</a>
+    </div>
+</div>
+</body>
+</html>
